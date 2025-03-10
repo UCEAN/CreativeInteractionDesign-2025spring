6e3490ae5f04a9a7d8c9436f5c7cd2c163384346
UCEAN <2903019970@qq.com>
2025-03-10 16:50:05 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,19 @@
+
+      document.write("hello world!");
+      function sayHello(){
+      var respons=prompt('What is your name');
+      alert('hello'+respons+' ,welcome to our game');
+    };
+
+      function add(a,b){
+        returna+b;
+      }
+      console.log(add(20,30));
+      console.log(add(158,20078));
+
+         //sayHello();
+      function showTopic(){
+        var x =document.getElementById('demo');
+        x.style.fontSize="25px";
+        x.style.color="red";
+      }
